661f681bdbaafabb7a8a1f5ebfdb368c0f82abb0
Alex Hennings <blackboxlogic@gmail.com>
2025-05-05 15:56:41 -0400

    Return events as json, format client-side.

--- ProxyByRegex/Proxy.cs
+++ ProxyByRegex/Proxy.cs
@@ -49,23 +49,28 @@ namespace ProxyByRegex
 			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
 			ILogger log)
 		{
+			var url = req.Query["url"].ToString();
+
+			if (url.StartsWith(@"https://docs.google.com/document")) { }
+			if (url.StartsWith(@"https://calendar.google.com/calendar/ical")) { }
+			else
+			{
+				throw new Exception("Unsupported URL");
+			}
+
 			using (HttpClient client = new HttpClient())
 			{
 				//req.Headers["Accepted"]
 				client.DefaultRequestHeaders.Add("Accept", @"*/*");
-				using (HttpResponseMessage remoteResponse = await client.GetAsync(req.Query["url"]))
+				using (HttpResponseMessage remoteResponse = await client.GetAsync(url))
 				using (HttpContent remoteContent = remoteResponse.Content)
 				{
 					var remoteContentString = await remoteContent.ReadAsStringAsync();
 
-					if (req.Query["url"].ToString().StartsWith(@"https://docs.google.com/document"))
+					if (url.StartsWith(@"https://docs.google.com/document"))
 					{
 						remoteContentString = new Regex(GoogleRedirect).Replace(remoteContentString, "");
 						remoteContentString = new Regex(HtmlHeadElement).Replace(remoteContentString, "");
-					}
-					else
-					{
-						throw new Exception("Unknown URL type");
 					}
 
 					var proxyResponse = new ContentResult
@@ -82,59 +87,8 @@ namespace ProxyByRegex
 			}
 		}
 
-		[FunctionName("GetNextEvent")]
-		public static async Task<IActionResult> GetNextEvent(
-			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
-			HttpRequest req)
-		{
-			using (HttpClient client = new HttpClient())
-			{
-				client.DefaultRequestHeaders.Add("Accept", @"*/*");
-				using (HttpResponseMessage remoteResponse = await client.GetAsync(req.Query["url"]))
-				using (HttpContent remoteContent = remoteResponse.Content)
-				{
-					var remoteContentString = await remoteContent.ReadAsStringAsync();
-					var cal = Calendar.Load(remoteContentString);
-
-					var events = cal.GetOccurrences<CalendarEvent>(DateTime.Now, DateTime.Now.AddYears(1));
-					var nextEvents = events
-						.OrderBy(e => e.Period.StartTime)
-						.Select(e => e.Source)
-						.OfType<CalendarEvent>();
-
-					foreach (var contains in req.Query["contains"])
-					{
-						nextEvents = nextEvents.Where(e => e.Summary.Contains(contains, StringComparison.InvariantCultureIgnoreCase)
-						|| e.Description.Contains(contains, StringComparison.InvariantCultureIgnoreCase)).ToArray();
-					}
-
-					var nextEvent = nextEvents.FirstOrDefault();
-
-					var description = "<span>no upcoming events found :(</span>";
-
-					if (nextEvent != null)
-					{
-						description = $"<h1 id='eventDateId' class='eventDateClass'>{nextEvent.Start.Date.ToString("dddd, MMMM d, yyyy")}</h1><h2 id='eventSummaryId' class='eventSummaryClass'>{nextEvent.Summary}</h2><div id='eventDescriptionId' class='eventDescriptionClass' style='eventDescriptionStyle'>{nextEvent.Description}</div>";
-						description = description.Replace("<ul>", "<ul style='list-style: inside'>"); // carrd has list-style:none on <ul>.
-						description = description.Replace("<b>", "<b style='font-weight: bolder'>");
-					}
-
-					var proxyResponse = new ContentResult
-					{
-						Content = description,
-						ContentType = "text/html; charset=utf-8",
-						StatusCode = (int)HttpStatusCode.OK
-					};
-
-					AddStandardResponseHeaders(req.HttpContext.Response, req);
-
-					return proxyResponse;
-				}
-			}
-		}
-
-		[FunctionName("GetNextEventsSummaries")]
-		public static async Task<IActionResult> GetNextEventsSummaries(
+		[FunctionName("GetNextEventsJSON")]
+		public static async Task<IActionResult> GetNextEventsJSON(
 			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
 			HttpRequest req)
 		{
@@ -149,104 +103,29 @@ namespace ProxyByRegex
 
 					var months = int.Parse(req.Query["months"].FirstOrDefault("12"));
 
-					var events = cal.GetOccurrences<CalendarEvent>(DateTime.Now.Date.AddDays(1), DateTime.Now.AddMonths(months));
-					var nextEvents = events.OrderBy(e => e.Period.StartTime).ToArray();
+					var events = cal.GetOccurrences<CalendarEvent>(DateTime.Now, DateTime.Now.AddMonths(months));
+					var nextEvents = events
+						.OrderBy(e => e.Period.StartTime)
+						.Select(e => e.Source)
+						.OfType<CalendarEvent>();
 
 					foreach (var contains in req.Query["contains"])
 					{
-						nextEvents = nextEvents.Where(e => (e.Source as CalendarEvent).Summary.Contains(contains, StringComparison.InvariantCultureIgnoreCase)
-						|| (e.Source as CalendarEvent).Description.Contains(contains, StringComparison.InvariantCultureIgnoreCase)).ToArray();
+						nextEvents = nextEvents.Where(e => e.Summary.Contains(contains, StringComparison.InvariantCultureIgnoreCase)
+						|| e.Description?.Contains(contains, StringComparison.InvariantCultureIgnoreCase) == true).ToArray();
 					}
 
-					var html = "<span id='eventSummaryId' class='eventSummaryClass'>No events found :(</span>";
-
-					if (nextEvents.Any())
+					foreach (var e in nextEvents)
 					{
-						html = "<ul>" + string.Concat(nextEvents.Select(e => $"<li><span id='eventSummaryId' class='eventSummaryClass'><b>{e.Period.StartTime.Date.ToString("ddd, MMM d, yyyy")}</b> {(e.Source as CalendarEvent).Summary}</span></li>")) + "</ul>";
-						html = html.Replace("TBD", "<b>TBD</b>");
+						e.Description = e.Description?.Replace("<ul>", "<ul style='list-style: inside'>").Replace("<b>", "<b style='font-weight: bolder'>"); // carrd has list-style:none on <ul>.
 					}
 
-					var proxyResponse = new ContentResult
-					{
-						Content = html,
-						ContentType = "text/html; charset=utf-8",
-						StatusCode = (int)HttpStatusCode.OK
-					};
-
-					AddStandardResponseHeaders(req.HttpContext.Response, req);
-
-					return proxyResponse;
-				}
-			}
-		}
-
-		[FunctionName("GetOtherDances")]
-		public static async Task<IActionResult> GetOtherDances(
-			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
-			HttpRequest req)
-		{
-			var state = " " + req.Query["state"]; // like "ME"
-
-			using (HttpClient client = new HttpClient())
-			{
-				client.DefaultRequestHeaders.Add("Accept", @"*/*");
-				using (HttpResponseMessage remoteResponse = await client.GetAsync("https://www.trycontra.com/dances_locs.json"))
-				using (HttpContent remoteContent = remoteResponse.Content)
-				{
-					var remoteContentString = await remoteContent.ReadAsStringAsync();
-					var dances = JsonSerializer.Deserialize<List<DanceSeries>>(remoteContentString)
-						.Where(d => d.city.EndsWith(state) && !d.inactive)
-						.OrderBy(d=> d.lat + d.lon) // south east to north west
-						.ToArray();
-
-					var html = "<div id='otherDancesListId' class='otherDancesListClass'>" + string.Join(", ", dances
-						.Select(d => $"<a href=\"{d.url}\">{d.city.Replace(state, "").TrimEnd(',')}</a>")) + "</div>";
-
-					var proxyResponse = new ContentResult
-					{
-						Content = html,
-						ContentType = "text/html; charset=utf-8",
-						StatusCode = (int)HttpStatusCode.OK
-					};
-
-					AddStandardResponseHeaders(req.HttpContext.Response, req);
-
-					return proxyResponse;
-				}
-			}
-		}
-
-		[FunctionName("GetFilterCalendar")]
-		public static async Task<IActionResult> GetFilterCalendar(
-			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
-			HttpRequest req)
-		{
-			using (HttpClient client = new HttpClient())
-			{
-				client.DefaultRequestHeaders.Add("Accept", @"*/*");
-				using (HttpResponseMessage remoteResponse = await client.GetAsync(req.Query["url"]))
-				using (HttpContent remoteContent = remoteResponse.Content)
-				{
-					var remoteContentString = await remoteContent.ReadAsStringAsync();
-					var cal = Calendar.Load(remoteContentString);
-
-					//var months = int.Parse(req.Query["months"].FirstOrDefault("12"));
-
-					foreach (var e in cal.Events.ToArray())
-					{
-						if (req.Query["contains"].Any(contains => !e.Summary.Contains(contains, StringComparison.InvariantCultureIgnoreCase)
-							&& !e.Description.Contains(contains, StringComparison.InvariantCultureIgnoreCase)))
-						{
-							cal.Events.Remove(e);
-						}
-					}
-
-					string result = new CalendarSerializer().SerializeToString(cal);
+					var result = JsonSerializer.Serialize(nextEvents.Select(e => new DanceEvent() { date = e.Start.AsDateTimeOffset, summary = e.Summary, description = e.Description }).ToArray());
 
 					var proxyResponse = new ContentResult
 					{
 						Content = result,
-						ContentType = "text/calendar; charset=UTF-8",
+						ContentType = "application/json; charset=utf-8",
 						StatusCode = (int)HttpStatusCode.OK
 					};
 
@@ -256,6 +135,83 @@ namespace ProxyByRegex
 				}
 			}
 		}
+
+		//[FunctionName("GetOtherDances")]
+		//public static async Task<IActionResult> GetOtherDances(
+		//	[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
+		//	HttpRequest req)
+		//{
+		//	var state = " " + req.Query["state"]; // like "ME"
+
+		//	using (HttpClient client = new HttpClient())
+		//	{
+		//		client.DefaultRequestHeaders.Add("Accept", @"*/*");
+		//		using (HttpResponseMessage remoteResponse = await client.GetAsync("https://www.trycontra.com/dances_locs.json"))
+		//		using (HttpContent remoteContent = remoteResponse.Content)
+		//		{
+		//			var remoteContentString = await remoteContent.ReadAsStringAsync();
+		//			var dances = JsonSerializer.Deserialize<List<DanceSeries>>(remoteContentString)
+		//				.Where(d => d.city.EndsWith(state) && !d.inactive)
+		//				.OrderBy(d=> d.lat + d.lon) // south east to north west
+		//				.ToArray();
+
+		//			var html = "<div id='otherDancesListId' class='otherDancesListClass'>" + string.Join(", ", dances
+		//				.Select(d => $"<a href=\"{d.url}\">{d.city.Replace(state, "").TrimEnd(',')}</a>")) + "</div>";
+
+		//			var proxyResponse = new ContentResult
+		//			{
+		//				Content = html,
+		//				ContentType = "text/html; charset=utf-8",
+		//				StatusCode = (int)HttpStatusCode.OK
+		//			};
+
+		//			AddStandardResponseHeaders(req.HttpContext.Response, req);
+
+		//			return proxyResponse;
+		//		}
+		//	}
+		//}
+
+		//[FunctionName("GetFilterCalendar")]
+		//public static async Task<IActionResult> GetFilterCalendar(
+		//	[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
+		//	HttpRequest req)
+		//{
+		//	using (HttpClient client = new HttpClient())
+		//	{
+		//		client.DefaultRequestHeaders.Add("Accept", @"*/*");
+		//		using (HttpResponseMessage remoteResponse = await client.GetAsync(req.Query["url"]))
+		//		using (HttpContent remoteContent = remoteResponse.Content)
+		//		{
+		//			var remoteContentString = await remoteContent.ReadAsStringAsync();
+		//			var cal = Calendar.Load(remoteContentString);
+
+		//			//var months = int.Parse(req.Query["months"].FirstOrDefault("12"));
+
+		//			foreach (var e in cal.Events.ToArray())
+		//			{
+		//				if (req.Query["contains"].Any(contains => !e.Summary.Contains(contains, StringComparison.InvariantCultureIgnoreCase)
+		//					&& !e.Description.Contains(contains, StringComparison.InvariantCultureIgnoreCase)))
+		//				{
+		//					cal.Events.Remove(e);
+		//				}
+		//			}
+
+		//			string result = new CalendarSerializer().SerializeToString(cal);
+
+		//			var proxyResponse = new ContentResult
+		//			{
+		//				Content = result,
+		//				ContentType = "text/calendar; charset=UTF-8",
+		//				StatusCode = (int)HttpStatusCode.OK
+		//			};
+
+		//			AddStandardResponseHeaders(req.HttpContext.Response, req);
+
+		//			return proxyResponse;
+		//		}
+		//	}
+		//}
 
 		private static void AddStandardResponseHeaders(HttpResponse response, HttpRequest req)
 		{
@@ -275,6 +231,13 @@ namespace ProxyByRegex
 			public string[] icals { get; set; }
 			public double lat { get; set; }
 			public double lon { get; set; }
+		}
+
+		public class DanceEvent
+		{
+			public DateTimeOffset date { get; set; }
+			public string summary { get; set; }
+			public string description { get; set; }
 		}
 	}
 }
