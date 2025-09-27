8b9e2b1289bd1b519eb295d165448a860dd87ad7
Alex Hennings <blackboxlogic@gmail.com>
2025-09-26 21:26:29 -0400

    Inject httpClient.

--- ProxyByRegex/Program.cs
+++ ProxyByRegex/Program.cs
@@ -10,5 +10,6 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+builder.Services.AddHttpClient();
 
 builder.Build().Run();
