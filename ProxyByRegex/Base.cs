cf02b0c0da6115dfdaa7b5d4c3e25feef475636b
Alex Hennings <blackboxlogic@gmail.com>
2025-10-30 14:32:52 -0400

    Send preview email to single recipient, with a link to send-all

--- ProxyByRegex/Base.cs
+++ ProxyByRegex/Base.cs
@@ -26,6 +26,11 @@ public abstract class Base
 		Configuration = configuration;
 	}
 
+	internal string GetConfigOrThrow(string key)
+	{
+		return Configuration[key] ?? throw new Exception("Missing config: " + key);
+	}
+
 	internal async Task<DanceEvent[]> GetNextEvents(string urlString, string[] containsFilters, int months = 12)
 	{
 		var remoteContentString = await Fetch(urlString);
@@ -37,21 +42,23 @@ public abstract class Base
 			.OrderBy(e => e.Period.StartTime)
 			// source is the INITIAL reoccuring event
 			.Where(e => e.Source is CalendarEvent)
-			.Select(e => new {period = e.Period, source = (CalendarEvent)e.Source})
+			.Select(e => new { period = e.Period, source = (CalendarEvent)e.Source })
 			.Where(e =>
 				containsFilters.Length == 0 ||
 				containsFilters.Any(contains =>
 					e.source.Summary?.Contains(contains, StringComparison.InvariantCultureIgnoreCase) == true ||
 					e.source.Description?.Contains(contains, StringComparison.InvariantCultureIgnoreCase) == true)
 			)
-			.Select(e => new DanceEvent() {
+			.Select(e => new DanceEvent()
+			{
 				date = new DateTimeOffset(e.period.StartTime.AsUtc),
 				start = new DateTimeOffset(e.period.StartTime.AsUtc),
 				end = new DateTimeOffset(e.period.EffectiveEndTime?.AsUtc ?? e.period.StartTime.AsUtc),
 				summary = e.source.Summary ?? "",
 				// carrd has list-style:none on <ul>.
 				description = e.source.Description?.Replace("<ul>", "<ul style='list-style: inside'>")?.Replace("<b>", "<b style='font-weight: bolder'>")?.Replace("\n", "<br>"),
-				location = e.source.Location})
+				location = e.source.Location
+			})
 			.ToArray();
 
 		return nextEvents;
--- ProxyByRegex/Email.cs
+++ ProxyByRegex/Email.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace CalendarFunctions;
 
+// Send preview to me, click link to send all.
 public class Email : Base
 {
 	public Email(IHttpClientFactory httpClientFactory, ILogger<Proxy> logger, IMemoryCache cache, Microsoft.Extensions.Configuration.IConfiguration configuration)
@@ -16,64 +17,83 @@ public class Email : Base
 	{
 	}
 
-	[Function("SendMonthlyEventEmail")]
-	public async Task<IActionResult> SendMonthlyEventEmail(
-		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
-	//public async Task SendMonthlyEventEmail([TimerTrigger("0 40 15 1 * *")] TimerInfo myTimer)
+	[Function("SendEmailToSubscribersApi")]
+	public async Task<IActionResult> SendEmailToSubscribersApi([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
 	{
-		var isProduction = Convert.ToBoolean(Environment.GetEnvironmentVariable("IsProduction"));
+		var isProd = Environment.GetEnvironmentVariable("Environment") == "PROD"; // DEV, TEST, PROD
 
-		if (!isProduction)
+		if (!isProd)
 		{
 			Logger.LogInformation("Not in production slot, skipping email.");
-			return new OkResult();
+			return new ForbidResult();
 		}
 
-		var nextEvents = await GetNextEvents("https://calendar.google.com/calendar/ical/0d91bca8eebb5bf2b86e7ea2ef26a3f6f1729ee3c73b87985a0407204e00dbc4%40group.calendar.google.com/public/basic.ics", ["contra"], 1);
-		var content = nextEvents.Select(e => $"{e.date:MMMM dd}: {e.summary} at {e.location}<br>{e.description}").Aggregate((a, b) => a + "<br><br>" + b);
 		var subscribers = await GetMailchimpSubscribers(
-			Configuration["MailChimpApiKey"] ?? throw new Exception("Missing MailChimpApiKey"),
-			Configuration["MailChimpAudienceId"] ?? throw new Exception("Missing MailChimpAudienceId"));
-		//test to one person:
-		subscribers = new string[] { "blackboxlogic@gmail.com" };
-		foreach (var subscriber in subscribers)
-		{
-			await SendEmailAsync(subscriber, "Upcoming Dances", content);
-		}
+			GetConfigOrThrow("MailChimpApiKey"),
+			GetConfigOrThrow("MailChimpAudienceId"));
+
+		//await SendEmailFromGmail(subscribers, req.Query["CalendarUrl"].ToString());
+
 		return new OkResult();
 	}
 
-	private async Task SendEmailAsync(string recipient, string subject, string body)
+	[Function("SendSampleEmailTimer")] // At 10:10 AM on day 15 of every month (after second sundays)
+	public async Task SendSampleEmailTimer([TimerTrigger("0 10 10 15 * *")] TimerInfo myTimer)
 	{
-		var fromAddress = new MailAddress(Configuration["GmailUser"] ?? throw new Exception("Missing GmailUser"), "Common Floor Contra Dance");
-		var toAddress = new MailAddress(recipient);
-		var smtp = new SmtpClient
+		var domain = Environment.GetEnvironmentVariable("Domain") ?? throw new Exception("Missing Domain env var");
+		var calendarUrl = GetConfigOrThrow("CalendarUrl");
+		var subscribers = new[] { (GetConfigOrThrow("EmailApprover"), $"<a href='https://{domain}/api/SendMonthlyEventEmailApi?CalendarUrl={calendarUrl}'>Send to all subscribers</a>") };
+		await SendEmailFromGmail(subscribers, calendarUrl);
+	}
+
+	private async Task SendEmailFromGmail((string emailAddress, string footer)[] subscribers, string calendarUrl)
+	{
+		var nextEvents = await GetNextEvents(calendarUrl, ["contra"], 1);
+
+		var content = nextEvents.Select(e => $"{e.date:MMMM dd}: {e.summary} at {e.location}<br>{e.description}").Aggregate((a, b) => a + "<br><br>" + b);
+
+		var fromAddress = new MailAddress(GetConfigOrThrow("GmailSender"), GetConfigOrThrow("SeriesName"));
+
+		using (var smtp = new SmtpClient
 		{
 			Host = "smtp.gmail.com",
 			Port = 587,
 			EnableSsl = true,
 			DeliveryMethod = SmtpDeliveryMethod.Network,
 			UseDefaultCredentials = false,
-			Credentials = new NetworkCredential(fromAddress.Address, Configuration["GmailAppPassword"] ?? throw new Exception("Missing GmailAppPassword"))
-		};
-		using (var message = new MailMessage(fromAddress, toAddress)
-		{
-			Subject = subject,
-			Body = body,
-			IsBodyHtml = true
+			Credentials = new NetworkCredential(fromAddress.Address, GetConfigOrThrow("GmailSenderAppPassword"))
 		})
 		{
-			await smtp.SendMailAsync(message);
+			foreach (var subscriber in subscribers)
+			{
+				var personalizedContent = content + subscriber.footer;
+				using (var message = new MailMessage(fromAddress, new MailAddress(subscriber.emailAddress))
+				{
+					Subject = "Upcoming Dances",
+					Body = personalizedContent,
+					IsBodyHtml = true
+				})
+				{
+					await smtp.SendMailAsync(message);
+				}
+				await Task.Delay(1000); // 1 second delay
+			}
 		}
 	}
 
-	private async Task<IEnumerable<string>> GetMailchimpSubscribers(string apiKey, string listId)
+	// unsubscribe link like: https://dance.us5.list-manage.com/profile?u=a1f02de7f307d697cbefdaf35&id=befe34b442&e=920e49ee2d
+	private async Task<(string emailAddress, string footer)[]> GetMailchimpSubscribers(string apiKey, string listId)
 	{
 		var mailChimpManager = new MailChimp.Net.MailChimpManager(apiKey);
-		var members = await mailChimpManager.Members.GetAllAsync(listId);
+		var apiInfoTask = mailChimpManager.Api.GetInfoAsync();
+		var membersTask = mailChimpManager.Members.GetAllAsync(listId);
+		await Task.WhenAll(apiInfoTask, membersTask);
+		var apiInfo = await apiInfoTask;
+		var members = await membersTask;
+
 		return members
 			.Where(m => m.Status == MailChimp.Net.Models.Status.Subscribed)
-			.Select(m => m.EmailAddress)
+			.Select(m => (m.EmailAddress, "<br><br>" + $"<a href='https://dance.us5.list-manage.com/profile?u={apiInfo.AccountId}&id={m.ListId}&e={m.UniqueEmailId}'>Manage your subscription</a>."))
 			.ToArray();
 	}
 }
