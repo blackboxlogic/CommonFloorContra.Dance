e45b1a9d6a2b559151d358a6d06d5202834a8f03
Alex Hennings <Alex>
2026-08-15 16:17:37 -0400

    Automate kennebec email

--- ProxyByRegex/Email.cs
+++ ProxyByRegex/Email.cs
@@ -13,6 +13,7 @@ namespace CalendarFunctions;
 
 // TODO
 // Set for multiple dance series (put config in json list, include email template maybe as a link hosted on their site)
+// I guess their site could host their config file if it was encrypted?
 // Filter: CANCELLED for example
 
 // Schedule a preview event email sent to me with a link to send the email to all mailchimp subscribers.
@@ -20,9 +21,44 @@ namespace CalendarFunctions;
 public class Email(IHttpClientFactory httpClientFactory, ILogger<Proxy> logger, IMemoryCache cache, IConfiguration configuration)
 	: Base(httpClientFactory, logger, cache, configuration)
 {
+	// At Noon-oh-five on the 2nd saturday of the month.
+	[Function("SendKennebecEmailTimer")]
+	//public async Task SendKennebecEmailTimer([TimerTrigger("0 05 17 8-14 * SAT", RunOnStartup = true)] TimerInfo myTimer)
+	public async Task SendKennebecEmailTimer([TimerTrigger("0 05 17 8-14 * SAT")] TimerInfo myTimer)
+	{
+		var email = new EmailModel
+		{
+			SeriesName = "Kennebec Contra Dance",
+			SeriesWebsite = "https://upcoming.kennebec-contra.org",
+			CalendarUrl = "https://calendar.google.com/calendar/ical/365bea0a69c61ed1419ea490097d7311c8461a001f47cfebb28b0891e3ecbb5c%40group.calendar.google.com/public/basic.ics",
+			LightColor = "#cfe2f3",
+			DarkColor = "#294d78",
+			PopColor = "#ffd600",
+			ToAddress = "KCD-News@kennebec-contra.org",
+			FromAddress = GetConfigOrThrow("SeriesGmailSender"),
+			FromAddressAppUser = GetConfigOrThrow("SeriesGmailSenderUser"),
+			FromAddressAppPassword = GetConfigOrThrow("SeriesGmailSenderAppPassword"),
+			Build = BuildTime + Configuration["Environment"]
+		};
 
-	[Function("SendSampleEmailTimer")] // At Noon-oh-five every 1st day of the month
-	public async Task SendSampleEmailTimer([TimerTrigger("0 05 17 1 * *")] TimerInfo myTimer)
+		(var cal, _, _) = await GetNextEvents(email.CalendarUrl, [], 1);
+		email.Events = cal.events;
+		email.FaviconUrl = await GetFaviconUrlFromWebsite(email.SeriesWebsite);
+		email.Subject = $"{email.SeriesName} ~ Upcoming Events";
+
+		if (email.Events.Length > 0)
+		{
+			await SendEmailFromGmail(email);
+		}
+		else
+		{
+			Logger.LogInformation($"No events to email in {DateTime.Now:MMMM}.");
+		}
+	}
+
+	// At Noon-oh-five every 1st day of the month.
+	[Function("SendCFCDEmailTimer")]
+	public async Task SendCFCDEmailTimer([TimerTrigger("0 05 17 1 * *"/*, RunOnStartup = true*/)] TimerInfo myTimer)
 	{
 		try
 		{
@@ -143,21 +179,27 @@ public class Email(IHttpClientFactory httpClientFactory, ILogger<Proxy> logger, 
 
 	private static async Task<string> GetFaviconUrlFromWebsite(string siteUrl)
 	{
+		var baseUri = new Uri(siteUrl);
+
 		try
 		{
 			using var client = new HttpClient();
-			var html = await client.GetStringAsync(siteUrl);
+			using var response = await client.GetAsync(siteUrl);
+			response.EnsureSuccessStatusCode();
+			baseUri = response.RequestMessage?.RequestUri ?? baseUri;
+
+			var html = await response.Content.ReadAsStringAsync();
 			var match = System.Text.RegularExpressions.Regex.Match(html,
 				@"<link[^>]+rel=""[^""]*icon[^""]*""[^>]+href=""([^""]+)""",
 				System.Text.RegularExpressions.RegexOptions.IgnoreCase);
 			if (match.Success)
 			{
 				var href = match.Groups[1].Value;
-				return href.StartsWith("http") ? href : new Uri(new Uri(siteUrl), href).ToString();
+				return href.StartsWith("http") ? href : new Uri(baseUri, href).ToString();
 			}
 		}
 		catch { }
-		return new Uri(new Uri(siteUrl), "/favicon.ico").ToString();
+		return new Uri(baseUri, "/favicon.ico").ToString();
 	}
 
 	public class EmailModel
